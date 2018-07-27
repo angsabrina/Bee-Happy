dfadc8fe8fc2c12ef28a58355e7345c942cd2339
unknown <sabrina.ang@gatech.edu>
2018-07-27 01:24:31 -0400

    start work on store need to figure out structure

---
+++ Bee Happy/Assets/Scripts/Interactibles/Interactible.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactible: MonoBehaviour {
+
+    public GameObject highlighter;
+    GameObject player;
+    bool highlighted;
+    bool proximityGood;
+    PlayerController playerController;
+    float radius = 0.3f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+        proximityGood = true;
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        proximityGood = false;
+    }
+
+    public virtual void OnMouseOver()
+    {
+        if(proximityGood)
+        {
+            if (!highlighted)
+            {
+                Instantiate(highlighter, gameObject.transform);
+                highlighted = true;
+            }
+        } 
+    }
+
+    //void OnMouseExit()
+    //{
+    //    if (proximityGood)
+    //    {
+    //        if (highlighted)
+    //        {
+    //            highlighted = false;
+    //            highlighter.SetActive(false);
+    //            //Destroy(highlighter);
+    //        }
+    //    }
+    //}
+}
---
+++ Bee Happy/Assets/Scripts/Interactibles/PickUps/Flowers/Flower.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flower : Interactible{
+
+    //public GameObject highlighter;
+    Text flowerPickUp;
+    protected int flowerXP;
+    bool highlighted;
+    float radius = 0.3f;
+
+    //void Start()
+    //{
+    //    player = GameObject.FindGameObjectWithTag("Player");
+    //    playerController = player.GetComponent<PlayerController>();
+    //}
+
+    //void OnTriggerStay(Collider collision)
+    //{
+    //    //Debug.Log(flowerXP);
+    //    proximityGood = true;
+    //}
+
+    //void OnTriggerExit(Collider collision)
+    //{
+    //    proximityGood = false;
+    //}
+
+    public override void OnMouseOver()
+    {
+        base.OnMouseOver();
+        PlayerReach(flowerXP, gameObject.transform.position, radius);
+    }
+
+    void PlayerReach(int XP, Vector3 center, float radius)
+    {
+        Collider[] collisions = Physics.OverlapSphere(center, radius);
+        foreach (Collider a in collisions)
+        {
+                Debug.Log(a.gameObject.tag);
+            if (a.gameObject.tag == "Player")
+            {
+                a.gameObject.GetComponent<PlayerController>().ReachedItem(this.gameObject);
+            }
+        }
+    }
+
+    public int getFlowerXP()
+    {
+        return flowerXP;
+    }
+
+    public Image getFlowerImage()
+    {
+        return GetComponentInChildren<Image>() == null ? null : GetComponentInChildren<Image>();
+    }
+}
